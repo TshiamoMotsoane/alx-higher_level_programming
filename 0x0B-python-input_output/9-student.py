95027586c1db918dcc27c907511f0c5f6592d5d6
TshiamoMotsone <motsoanetshiamo@outlook.com>
2024-02-06 16:59:09 +0200

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a class student."""
+
+
+
+class Student:
+    """Defines a student by first_name, last_name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student.
+
+        Args:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    
+    def to_json(self):
+        """Retrieves a dictionary representation of a student."""
+        return self.__dict__
+
