889ffe3864def6fe1c55256aa68bc5774f7999fb
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2024-02-07 02:46:05 -0800

    Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,7 +10,8 @@ def inherits_from(obj, a_class):
         a_class: Class or type to compare against.
 
     Return:
-        bool: True if the object is an inherited instance of the specified class
+        bool: True if the object is an inherited instance
+        of the specified class
         otherwise False.
     """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
