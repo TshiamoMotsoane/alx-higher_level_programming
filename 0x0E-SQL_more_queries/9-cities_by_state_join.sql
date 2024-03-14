9b1709a9dbd4bf60301c9e5d75aaf35ddc356330
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2024-03-14 09:19:49 -0700

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- Lists all cities contained in the database hbtn_0d_usa.
 
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name ='California') ORDER BY id ASC;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
