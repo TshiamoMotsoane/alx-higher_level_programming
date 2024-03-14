ed13d9d8cfdb63ea664d9b68f2f0f51a0d4c9065
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2024-03-14 09:08:49 -0700

    Cities table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,7 @@
+-- Creates the database hbtn_0d_usa and the table cities.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Use a database
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL,
+state _id INT NOT NULL, name VARCHAR(256) NOT NULL, PRIMARY KEY(ID),
+FOREIGN KEY(state_id) REFERENCES states
