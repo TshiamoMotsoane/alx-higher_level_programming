a51d67d4ad7be486df31580b907cc7842eb0f7b7
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2024-03-14 08:44:43 -0700

    Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
--- Creates the database hbtn_0d_2 and the user user_0d_2.
+-- Creates the database hbtn_0d_2 and the user user_0d_2 and grants SELECT privileges to a user.
 
+CREATE A DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhosts IDENTIFIED BY 'user_0d_2_pwd';
-
 -- Grants SELECT privileges to a user
 GRANT SELECT privileges to a user
