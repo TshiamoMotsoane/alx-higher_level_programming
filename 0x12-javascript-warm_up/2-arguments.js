9a245242d24f03f755eeb9040edd7874789030ac
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2024-04-09 09:09:24 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length === 2) {
-	consol.log('No argument');
-}	else if (process.argv.length === 3) {
-	console.log('Argument fount');
-}	else {
-	console.log('Arguments found');
+  consol.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument fount');
+} else {
+  console.log('Arguments found');
 }
