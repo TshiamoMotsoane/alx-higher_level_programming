abc80884b21d0c5b80663b909cf0c781d413dab5
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2024-04-09 06:35:57 -0700

    Rectangle #3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,21 +1,24 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor (w, h) {
-		if ((w > 0) && (h > 0)) {
-			this.width = w;
-			this.height = h;
-		}
+  constructor (w, h) {
+   if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  print() {
+    if (Object.keys(this).length === 0) {
+      consol.log("Empty object");
+    } else {
+      for (let i = 0; i < this.height; i++) {
+	let row = '';
+	for (let j = 0; j < this.width; j++) {
+	  row += 'X';
 	}
-
-	print () {
-		for (let i = 0; i < this.height; i++) {
-			let row = '';
-			for (let j = 0; j < this.width; j++) {
-				row += 'X';
-			}
-			consol.log(row);
-		}
-	}
+	consol.log(row);
+     }
+   }
 }
 
 module.exports = Rectangle;
