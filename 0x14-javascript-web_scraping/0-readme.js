71002e72d4411695f9cb3bf98b7fa5f79827335b
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2024-05-21 09:45:43 -0700

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
 
+// Import the required 'fs' module to handle file system operations.
 const fs = require('fs');
 
+// Read the file specified by the first command-line argument.
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
 
+  //If an error occurs, log it to the console.
   if (err) {
     console.error('Error reading the file:', err);
   } else {
-
+    // If no error, print the file content to the console.
     console.log(data);
   }
 });
