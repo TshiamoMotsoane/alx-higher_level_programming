877088a2ce6c2a3665b8b987df284f0fa4696add
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2024-05-21 09:34:58 -0700

    Readme

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+
+  if (err) {
+    console.error('Error reading the file:', err);
+  } else {
+
+    console.log(data);
+  }
+});
