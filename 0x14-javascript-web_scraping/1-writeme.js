1af11175aa59098b24b0367d519934dee4d325d5
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2024-05-22 03:21:24 -0700

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -7,7 +7,7 @@ fs.writeFile(process.argv[2], process.argv[3], 'utf8', error => {
   // Write the file specified as the third command-line argument.
 
   if (error) {
-    // If an error occurs, the 'error' parameter will contain an error object.
-    console.error(error);
+  // If an error occurs, the 'error' parameter will contain an error object.
+  console.error(error);
     }
 });
