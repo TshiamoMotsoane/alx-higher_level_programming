96d6d7a6424db5b4a15ce79955dd81c106a6d0ea
TshiamoMotsoane <motsoanetshiamo@outlook.com>
2024-05-22 03:48:25 -0700

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,8 +2,16 @@
 
 const request = require('request');
 
+// Construct the URL using the provided episode number
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
+// Make a GET request to the URL
 request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+  if (error) {
+  console.error('Error:', error);
+  } else {
+
+  // Parse the response body and print the title of the film
+  console.log(JSON.parse(body).title);
+  }
 });
